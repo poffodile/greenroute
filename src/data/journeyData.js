383d637768e4a537f8786fab4718ecb77798cccd
poffodile <lamberville73@gmail.com>
2026-05-11 21:46:16 +0100

    Removed Hero component and styles; added Journey Planner component and data

---
+++ src/data/journeyData.js
@@ -0,0 +1,9 @@
+export const journeyPlannerData = {
+    fromPlaceholder: "Enter starting location",
+    toPlaceholder: "Enter destination",
+    date: "24 May 2025",
+    time: "09:00 AM",
+    preference: "All Transport",
+    buttonText: "Find Green Route",
+    ecoMessage: "Choosing public transport can reduce emissions by up to 70%.",
+  };
